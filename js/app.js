f56a36556d508081b2cdf416817cb88aa99e14ee
Giulio998 <giulio_favia@hotmail.it>
2024-03-01 13:12:42 +0100

    add console log description

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 let number = 100; // number
-console.log(number); //number
+console.log("number", number); //number
 
 
 const PI = 3.14; // number
-console.log(PI); //number
+console.log("PI", PI); //number
 
 //const PI = 3.1416 // number // non si può modificare una costante
 
 let radius = 8; //number
 let circle = (radius * PI * 2 ) //number
-console.log(circle); //number
+console.log("circle", circle); //number
 
 let name = "Carlo"; //string
 name = "Marco" //string
-console.log(name); //string
+console.log("name", name); //string
