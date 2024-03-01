8be425ff9dd0c1213d4fe91c97e1ea7d20789708
Giulio998 <giulio_favia@hotmail.it>
2024-03-01 13:08:48 +0100

    End

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let number = 100; // number
+console.log(number); //number
+
+
+const PI = 3.14; // number
+console.log(PI); //number
+
+//const PI = 3.1416 // number // non si può modificare una costante
+
+let radius = 8; //number
+let circle = (radius * PI * 2 ) //number
+console.log(circle); //number
+
+let name = "Carlo"; //string
+name = "Marco" //string
+console.log(name); //string
